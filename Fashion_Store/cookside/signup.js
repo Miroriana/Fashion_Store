3728ab0226fadbaddf9262dfb993b922c12d5694
alineuwera <uwerairakozealine@gmail.com>
2024-03-14 07:42:03 +0200

    I added validation in a form

--- Fashion_Store/cookside/signup.js
+++ Fashion_Store/cookside/signup.js
@@ -1,7 +1,55 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
-export default function SignUp() {
+export default function SignUp({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [confirmPasswordError, setConfirmPasswordError] = useState('');
+    const [passwordVisible, setPasswordVisible] = useState(true);
+
+    const isValidEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+
+    const validForm = () => {
+        let valid = true;
+        if (!email.trim()) {
+            setEmailError('email is required');
+            valid = false;
+
+        } else if (!isValidEmail(email)) {
+            setEmailError('email is invalid');
+            valid = false;
+        } else {
+            setEmailError('');
+        }
+
+        if (!password.trim()) {
+            setPasswordError('password is required');
+            valid = false
+        } else {
+            setPasswordError('');
+        }
+        if (password !== confirmPassword) {
+            setConfirmPasswordError("password doesn't match");
+            valid = false;
+        } else {
+            setConfirmPasswordError('');
+        } return valid
+    }
+
+    const handleSubmit = async () => {
+        if (validForm() === true) {
+            navigation.navigate("signIn")
+        }
+    }
+
     return (
         <View style={{ width: "100%", height: "100%" }}>
             <View style={{ marginLeft: 13 }}>
@@ -13,20 +61,25 @@ export default function SignUp() {
                 <Text style={{ marginBottom: 5, fontWeight: "bold" }}>Name</Text>
                 <TextInput style={{ backgroundColor: "whitesmoke", width: "95%", padding: 7, borderRadius: 10, borderWidth: 1, borderColor: "#EB9800" }}></TextInput>
 
-                <Text style={{ marginBottom: 5, fontWeight: "bold",marginTop:15}}>Email</Text>
+                <Text style={{ marginBottom: 5, fontWeight: "bold", marginTop: 15 }}>Email</Text>
+                <TextInput style={{ backgroundColor: "whitesmoke", width: "95%", padding: 7, borderRadius: 10, borderWidth: 1, borderColor: "#EB9800" }} onChangeText={(e) => { setEmail(e) }}>
+                </TextInput>
+                {emailError ? <Text style={{ color: 'red', paddingTop: 2 }}>{emailError}</Text> : null}
+
+                <Text style={{ marginBottom: 5, fontWeight: "bold", marginTop: 15 }}>Phone Number</Text>
                 <TextInput style={{ backgroundColor: "whitesmoke", width: "95%", padding: 7, borderRadius: 10, borderWidth: 1, borderColor: "#EB9800" }}></TextInput>
 
-                <Text style={{ marginBottom: 5, fontWeight: "bold",marginTop:15}}>Phone Number</Text>
-                <TextInput style={{ backgroundColor: "whitesmoke", width: "95%", padding: 7, borderRadius: 10, borderWidth: 1, borderColor: "#EB9800" }}></TextInput>
+                <Text style={{ marginBottom: 5, fontWeight: "bold", marginTop: 15 }}>Password</Text>
+                <TextInput style={{ backgroundColor: "whitesmoke", width: "95%", padding: 7, borderRadius: 10, borderWidth: 1, borderColor: "#EB9800" }} secureTextEntry onChangeText={(p) => { setPassword(p) }}></TextInput>
+                {passwordError ? <Text style={{ color: 'red', paddingTop: 2 }}>{passwordError}</Text> : null}
+                <AntDesign name='eyeo'></AntDesign>
 
-                <Text style={{ marginBottom: 5, fontWeight: "bold",marginTop:15}}>Password</Text>
-                <TextInput style={{ backgroundColor: "whitesmoke", width: "95%", padding: 7, borderRadius: 10, borderWidth: 1, borderColor: "#EB9800" }}></TextInput>
-
-                <Text style={{ marginBottom: 5, fontWeight: "bold",marginTop:15}}>Confirm Password</Text>
-                <TextInput style={{ backgroundColor: "whitesmoke", width: "95%", padding: 7, borderRadius: 10, borderWidth: 1, borderColor: "#EB9800" }}></TextInput>
+                <Text style={{ marginBottom: 5, fontWeight: "bold", marginTop: 15 }}>Confirm Password</Text>
+                <TextInput style={{ backgroundColor: "whitesmoke", width: "95%", padding: 7, borderRadius: 10, borderWidth: 1, borderColor: "#EB9800" }} secureTextEntry={passwordVisible} onChangeText={(cp) => { setConfirmPassword(cp) }}></TextInput>
+                {confirmPasswordError ? <Text style={{ color: 'red', paddingTop: 2 }}>{confirmPasswordError}</Text> : null}
             </View>
-            <TouchableOpacity style={{backgroundColor:"#EB9800",width:"91%",marginBottom:100,display:"flex",justifyContent:"center",alignSelf:"center",borderRadius:30,marginTop:30}}>
-                <Text style={{color:"white",padding:12,textAlign:"center",fontWeight:'400'}}>Sign Up</Text>
+            <TouchableOpacity onPress={()=>handleSubmit()} style={{ backgroundColor: "#EB9800", width: "91%", marginBottom: 100, display: "flex", justifyContent: "center", alignSelf: "center", borderRadius: 30, marginTop: 30 }}>
+                <Text style={{ color: "white", padding: 12, textAlign: "center", fontWeight: '400' }}>Sign Up</Text>
             </TouchableOpacity>
 
         </View>
